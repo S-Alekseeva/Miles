1069aea5adfa959f4a73edf13bc1ed6c0718b61e
Светлана Алексеева <Lanaleks90@gmail.com>
2022-11-21 20:53:09 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 10_000;
+        int a = 20;
+        int miles = ticket / a;
+        System.out.println("Бонусные мили: " + miles);
+
+    }
+}
